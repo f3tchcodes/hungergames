0d0ccf520c2ad67940f91ba47681260ac7f9b558
f3tch <f3tch.169@gmail.com>
2026-07-03 20:52:09 +0500

    saving the guild id and session cookie value inside database

--- src/interactions/host.ts
+++ src/interactions/host.ts
@@ -5,6 +5,7 @@ import {
 } from "discord.js";
 
 import config from "#utils/constants";
+import { games } from "#utils/db/schema";
 import type { MyInteractions } from "#utils/interfaces";
 
 // building the command
@@ -27,19 +28,27 @@ export default {
     async execute(client, interaction) {
         if (!interaction.isChatInputCommand()) return;
 
+        const guild_id = interaction.guildId;
+        if (guild_id === null) return interaction.reply({
+            content: "Failed to f3tch the guild ID."
+        });
+
         // f3tching the session cookie that we'll save
         // and use to send request to every endpoint
         const res = await fetch(`${config.BASE_URL}/hungergames/agree.php`);
-        res.headers.forEach(cookie => {
+        res.headers.forEach(async (cookie) => {
             if (cookie.startsWith("PHPSESSID")) {
                 const cRegex = /PHPSESSID=.*;/g;
                 const c = cookie.match(cRegex);
-                if (c === null || !c[0].startsWith("PHPSESSID=")) return interaction.reply({
-                    content: "Error occured while fetching the session cookie. Contact dev to fix.\nUsername: f3tch",
+                if (c === null) return interaction.reply({
+                    content: "Error occured while f3tching the session cookie. Contact dev to fix.\nUsername: f3tch",
                     flags: MessageFlags.Ephemeral
                 });
                 const cVal = c[0].replace("PHPSESSID=", "").slice(0, -1);
-                console.log(cVal);
+                await client.db.insert(games).values({
+                    guild_id: guild_id,
+                    session_id: cVal
+                });
             }
         });
     }

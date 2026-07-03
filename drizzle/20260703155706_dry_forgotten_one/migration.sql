cbbe0fecf3337c4c4c1732f9340ef73ac0fc97ae
f3tch <f3tch.169@gmail.com>
2026-07-03 20:57:58 +0500

    created column for channel id

---
+++ drizzle/20260703155706_dry_forgotten_one/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `games` ADD `channel_id` text;
--- src/utils/db/schema.ts
+++ src/utils/db/schema.ts
@@ -2,5 +2,6 @@ import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const games = sqliteTable("games", {
     guild_id: text().primaryKey(),
+    channel_id: text(),
     session_id: text()
 });

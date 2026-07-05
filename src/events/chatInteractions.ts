9dff8f5f700674f096e89090f61f1c0c355a1c63
f3tch <f3tch.169@gmail.com>
2026-07-05 12:05:25 +0500

    back to void i guess

--- src/events/chatInteractions.ts
+++ src/events/chatInteractions.ts
@@ -12,10 +12,11 @@ export default {
             await command?.execute(client, interaction, args);
         } catch (err) {
             console.log(`Error running command: ${err}`);
-            return await interaction.reply({
+            await interaction.reply({
                 content: "Error occured while running the command, contact the developer to fix!\nUsername: f3tch",
                 flags: MessageFlags.Ephemeral
             });
+            return;
         }
     }
 } satisfies MyEvents<"interactionCreate">;

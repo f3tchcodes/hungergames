50019b8a6975cf2b1d41fd6872e86d12162903f8
f3tch <f3tch.169@gmail.com>
2026-07-08 11:41:03 +0500

    not null and real boolean in district

--- src/utils/db/schema.ts
+++ src/utils/db/schema.ts
@@ -2,18 +2,19 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const games = sqliteTable("games", {
     guild_id: text().primaryKey(),
-    channel_id: text(),
+    channel_id: text().notNull(),
     session_id: text(),
-    tribute_size: integer(),
-    district_size: integer()
+    tribute_size: integer().notNull(),
+    district_size: integer().notNull()
 });
 
 export const districts = sqliteTable("districts", {
     guild_id: text().primaryKey(),
-    player_id: integer(),
-    district_id: integer(),
-    user_id: text(),
-    username: text(),
-    profile_pic_url: text(),
-    gender: text()
+    player_id: integer().notNull(),
+    district_id: integer().notNull(),
+    user_id: text().notNull(),
+    username: text().notNull(),
+    profile_pic_url: text().notNull(),
+    gender: text(),
+    real: integer() // there is no boolean in sqlite, so we're using integer 0 for false and 1 for true
 });

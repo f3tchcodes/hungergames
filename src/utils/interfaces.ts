273e22e766651d57129af0fc9bec37b2fe04b8f0
f3tch <f3tch.169@gmail.com>
2026-07-22 15:32:10 +0500

    cleaning code

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,4 +1,3 @@
-import type { Canvas } from "canvas-constructor/napi-rs";
 import { Client, type ClientEvents, type Interaction, type SlashCommandOptionsOnlyBuilder, type SlashCommandSubcommandsOnlyBuilder } from "discord.js";
 
 // for interactions
@@ -32,57 +31,6 @@ export interface RegisterPlayer {
 }
 
 // for building canvas list
-export interface PlayerslistInfo {
-    username: string;
-    player_id: number;
-    profile_pic_url: string;
-    district_id: number;
-    district_position: number;
-    real: boolean;
-}
-
-export interface ListCanvasGenerateInfo {
-    playerslistInfoChunks: PlayerslistInfo[][];
-
-    page: number;
-    district_size: number;
-    tribute_size: number;
-    includedefaultplayers: boolean | undefined;
-}
-
-export interface BuildListDistrictsData {
-    canvas: Canvas;
-    district_size: number;
-    page: number;
-    tribute_size: number;
-}
-
-export interface BuildListUserData extends BuildListDistrictsData {
-    playerslistInfoChunks: PlayerslistInfo[][];
-}
-
-export interface PaginatedDistricts {
-    district_name: string;
-    width: number;
-    height: number;
-}
-
-export interface PaginatedUsers {
-    real: boolean;
-    user: {
-        username: string;
-        width: number;
-        height: number;
-    };
-    profile: {
-        profile_pic_url: string;
-        position_width: number;
-        position_height: number;
-        size_width: number;
-        size_height: number;
-    };
-}
-
 export interface TributesReg {
     player_id: number;
     username: string;

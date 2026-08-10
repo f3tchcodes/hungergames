d8ecd0889d279901718a30e6831708b9823cdaea
f3tch <f3tch.169@gmail.com>
2026-08-11 00:52:34 +0500

    fixed page not reset on restart bug

--- src/commands/admin/restart.ts
+++ src/commands/admin/restart.ts
@@ -1,6 +1,8 @@
 import { PermissionsBitField, SlashCommandBuilder } from "discord.js";
+import { eq } from "drizzle-orm";
 
 import { _EphToast, getGamesTable, startGame } from "#utils/common";
+import { games } from "#utils/db/schema";
 import type { MyInteractions } from "#utils/interfaces";
 import { userPermissions } from "#utils/permissions";
 
@@ -27,6 +29,9 @@ export default {
 
         const qGames = await getGamesTable(interaction, guild_id);
         if (qGames[0] && !qGames[0].game_started) return await _EphToast(interaction, "Start a game before restarting!");
+
+        // set settings back to 0
+        await interaction.client.db.update(games).set({ game_page: 0, section_page: 0 }).where(eq(games.guild_id, guild_id));
 
         await startGame(interaction, guild_id, qGames);
         // the rest of the game would be played by /next command or auto mode

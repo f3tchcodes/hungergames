bca68dd1a504756b02701cb71652dbf843702148
f3tch <f3tch.169@gmail.com>
2026-08-11 19:21:10 +0500

    updating default events every time bot is online

--- src/utils/processGuilds.ts
+++ src/utils/processGuilds.ts
@@ -1,5 +1,7 @@
 import type { Client, Guild } from "discord.js";
 
+import { DEFAULT_EVENTS } from "#config/events";
+
 import { server_data } from "./db/schema.js";
 
 export async function processGuilds(client: Client, guild: Guild) {
@@ -11,6 +13,7 @@ export async function processGuilds(client: Client, guild: Guild) {
 
     const guilds = await client.db.select().from(server_data);
     const guildsId = guilds.map(server => server.guild_id);
+    await client.db.update(server_data).set({ events: DEFAULT_EVENTS });
     if (guildsId.includes(guildId)) return;
 
     await client.db.insert(server_data).values({ guild_id: guildId, guild_owner: ownerUsername, guild_name: guildName });
